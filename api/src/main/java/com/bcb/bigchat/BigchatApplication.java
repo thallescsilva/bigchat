ca8790da700f539878687ddc8fa77294b218e1c1
Thalles Silva <thallescezariosilva@gmail.com>
2026-06-19 09:39:20 -0300

    chore: scaffold Spring Boot project with Docker and env config

---
+++ api/src/main/java/com/bcb/bigchat/BigchatApplication.java
@@ -0,0 +1,13 @@
+package com.bcb.bigchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigchatApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/bcb/bigchat/BigchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcb.bigchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
